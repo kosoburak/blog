cde20ccbcfc35e2683f631d07484a699df5af1bc
kosoburak <kosoburak@gmail.com>
2015-12-13 20:50:26 +0100

    default avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts, dependent: :destroy
-  has_attached_file :avatar
+  has_attached_file :avatar,
+                    :default_url => ":style/default_avatar.png"
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 end
